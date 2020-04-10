b6324ce0157d4612014ce61deab9ac0af7581ee7
Narendra Sisodiya <narendra.sisodiya@syngenta.com>
2020-04-10 15:59:22 +0530

    added Draggable

--- src/index.js
+++ src/index.js
@@ -16,40 +16,42 @@ export const ExampleComponent = ({ text }) => {
     <div className={container}>
       <div className={heading}>React ArtBoard</div>
       <div className={zoomableContainer}>
-        <div className={`${infiniteCanvas}`}>
-          <Draggable>
+        <Draggable>
+          <div className={`${infiniteCanvas}`}>
+            <Draggable>
+              <div
+                style={{
+                  display: "inline-block",
+                  background: "white",
+                  padding: 4,
+                  border: "1px solid green"
+                }}
+              >
+                <p>Drag Me</p>
+                <p>
+                  <i className={star} />
+                  <i className={star} />
+                  <i className={star} />
+                </p>
+                <button>Click me</button>
+              </div>
+            </Draggable>
             <div
               style={{
-                display: "inline-block",
-                background: "white",
-                padding: 4,
-                border: "1px solid green"
+                position: "absolute",
+                top: 0,
+                right: 0
               }}
             >
-              <p>Drag Me</p>
+              <button>Click me</button>
               <p>
                 <i className={star} />
                 <i className={star} />
                 <i className={star} />
               </p>
-              <button>Click me</button>
             </div>
-          </Draggable>
-          <div
-            style={{
-              position: "absolute",
-              top: 0,
-              right: 0
-            }}
-          >
-            <button>Click me</button>
-            <p>
-              <i className={star} />
-              <i className={star} />
-              <i className={star} />
-            </p>
           </div>
-        </div>
+        </Draggable>
       </div>
     </div>
   );
