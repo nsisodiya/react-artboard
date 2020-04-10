7576fff320137a3d101f159e0dabeb4033a83460
Narendra Sisodiya <narendra.sisodiya@syngenta.com>
2020-04-10 16:14:04 +0530

    refactor

--- src/index.js
+++ src/index.js
@@ -2,53 +2,31 @@ import React from "react";
 import styles from "./styles.module.css";
 
 import Draggable from "react-draggable"; // The default
+import { Star } from "./components/Star/Star";
 
 export const ExampleComponent = ({ text }) => {
-  const {
-    container,
-    heading,
-    zoomableContainer,
-    infiniteCanvas,
-    star,
-    zoom
-  } = styles;
+  const { container, heading, zoomableContainer, infiniteCanvas } = styles;
   return (
     <div className={container}>
       <div className={heading}>React ArtBoard</div>
       <div className={zoomableContainer}>
         <div className={`${infiniteCanvas}`}>
           <Draggable>
-            <div
-              style={{
-                display: "inline-block",
-                background: "white",
-                padding: 4,
-                border: "1px solid green"
-              }}
-            >
-              <p>Drag Me</p>
-              <p>
-                <i className={star} />
-                <i className={star} />
-                <i className={star} />
-              </p>
-              <button>Click me</button>
+            <div>
+              <Star />
             </div>
           </Draggable>
-          <div
-            style={{
-              position: "absolute",
-              top: 0,
-              right: 0
-            }}
-          >
-            <button>Click me</button>
-            <p>
-              <i className={star} />
-              <i className={star} />
-              <i className={star} />
-            </p>
-          </div>
+          <Draggable>
+            <div
+              style={{
+                position: "absolute",
+                top: 0,
+                right: 0
+              }}
+            >
+              <Star />
+            </div>
+          </Draggable>
         </div>
       </div>
     </div>
